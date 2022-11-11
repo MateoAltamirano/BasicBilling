b431ff96d39cea106365abf8753f47ed667122ef
Mateo Altamirano <mateo.altamirano.1998@gmail.com>
2022-11-11 00:09:39 -0400

    chore: initial api controllers

---
+++ Backend/BasicBilling.API/Controllers/BillingController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicBilling.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class BillingController : ControllerBase
+    {
+  
+        [HttpGet("/search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetHistoryByCategory()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/pending")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetPendingByClientID()
+        {
+            return Ok();
+        }
+
+        [HttpPost("/bills")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult CreateBill()
+        {
+            return Ok();
+        }
+
+        [HttpPost("/pay")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult CreatePayment()
+        {
+            return Ok();
+        }
+    }
+}
