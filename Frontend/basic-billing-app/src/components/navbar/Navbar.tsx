b9d5a8c3289c05fc2037d234e9846c65cfeb8faa
Mateo Altamirano <mateo.altamirano.1998@gmail.com>
2022-11-13 01:11:57 -0400

    style: add logo

--- Frontend/basic-billing-app/src/components/navbar/Navbar.tsx
+++ Frontend/basic-billing-app/src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Flex, TabList } from '@chakra-ui/react';
+import { Divider, Flex, Image, TabList, Text } from '@chakra-ui/react';
 import { NAVBAR_WIDTH, SECTION } from '../../utils/constants';
 import { FiClock, FiCalendar } from 'react-icons/fi';
 import NavbarButton from './NavbarButton';
@@ -13,6 +13,13 @@ const Navbar = () => {
       shadow={'lg'}
       w={`${NAVBAR_WIDTH}px`}
     >
+      <Flex alignItems={'center'}>
+        <Image src={'/vite.svg'} width={'3rem'} />
+        <Text ml={'0.5rem'} fontSize={'1.5rem'} fontWeight={'semibold'}>
+          {'Basic Billing'}
+        </Text>
+      </Flex>
+      <Divider m={'1rem 0'} />
       <TabList border={'none'} display={'flex'} flexDir={'column'}>
         <NavbarButton icon={<FiClock size={'1.25rem'} />}>
           {SECTION.PaymentHistory}
